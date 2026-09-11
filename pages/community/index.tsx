bb3e62b28f8203888c62e0a4aabc832d69441022
daniel <azizbekrakhmonkulov01@gmail.com>
2026-09-11 18:25:32 +0900

    feat: integration header and footer via layouts

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -1,5 +1,18 @@
-const CommunityList = () => {
-    return <div>COMMUNITY LIST</div>
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+import { useState } from "react";
+
+const Community = () => {
+    console.log("community component pages router");
+
+    const [title, setTitle] = useState<string>("hello");
+    return (
+        <div>
+            Community{""}
+            <button onClick={() => alert("hello Mit")} style={{ margin: "15px" }}>
+                press me
+            </button>
+        </div>
+    );
 };
 
-export default CommunityList;
+export default withLayoutBasic(Community);
---
+++ pages/property/detail.tsx
@@ -0,0 +1,9 @@
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+import { Container, Stack } from "@mui/material";
+
+const PropertyDetail = () => {
+
+    return <Container>PROPERTY LIST</Container>;
+};
+
+export default withLayoutBasic(PropertyDetail);
